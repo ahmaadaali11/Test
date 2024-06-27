e210b12ee2da2cbf22e3afd888bb7b315c4ae206
Ahmad Ali <ahmaadaali11@gmail.com>
2024-06-27 13:17:47 +0300

    First commit

---
+++ src/main/java/Cartest.java
@@ -0,0 +1,22 @@
+public class Cartest {
+
+
+    String Ahmad;
+    String Sami;
+
+    public static void main(String[] args) {
+
+    }
+    public Cartest(String ahmad, String sami) {
+        Ahmad = ahmad;
+        Sami = sami;
+        Ahmad =sami+ahmad;
+        System.out.println(Ahmad);
+        System.out.println("Done");
+    }
+
+
+
+}
+
+
